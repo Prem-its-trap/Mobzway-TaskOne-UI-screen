389d3f8eb0ad308c2ab236812a15a232aa77b22c
Prem Bhati <yanshusinghal0143@gmail.com>
2023-02-20 10:02:31 +0530

    mostly done

---
+++ script.js
@@ -0,0 +1,10 @@
+window.onload = () => {
+  let a = document.getElementById("polo navbarDropdownMenuLink");
+  console.log(a);
+  onclick = () => {
+    if (a) {
+      a.style.backgroundColor = "red";
+    }
+  };
+  //   }
+};
